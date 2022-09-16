c7e9a36392216cc2f597fadec07ea022e7f71802
SoulNoxer <113703765+SoulNoxer@users.noreply.github.com>
2022-09-16 22:00:26 +0300

    1

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-echo "<a href='./index1.php'>asdad</a>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
