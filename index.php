38bda7e1650ca8843baa367c79b1fe07ccccef17
Xiakery <113703732+Xiakery@users.noreply.github.com>
2022-09-16 23:07:03 +0300

    §1

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
-echo "<a href='./index.html'>asdaqwed</a>"."<br>";
-echo "<a href='./index1.php'>asdad</a>";
+$employees = "some text";
 ?>
+<h1>List of Employees</h1>
+<li>
+        <ul><?php echo $employees ?></ul>
+</li>
+
+
+</body>
+</html>
+
+
+
