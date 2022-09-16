c64ae8c6da3a70911d443e45065eeae07c9fe828
Xiakery <113703732+Xiakery@users.noreply.github.com>
2022-09-16 22:25:54 +0300

    1

--- index.php
+++ index.php
@@ -2,17 +2,3 @@
 echo "<a href='./index.html'>asdaqwed</a>"."<br>";
 echo "<a href='./index1.php'>asdad</a>";
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Documentфивфив</title>
-</head>
-<body>
-    asdasdsadasd
-    <p>dsad</p>
-    <hr>
-</body>
-</html>
